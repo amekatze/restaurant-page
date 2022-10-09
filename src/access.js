9f900e4581f456c8a8b3f24211cd341e6de602f9
amekatze <setharie@gmail.com>
2022-10-09 15:40:57 +0900

    Correction of Typo

--- src/access.js
+++ src/access.js
@@ -2,7 +2,7 @@ function access(){
     const main = document.querySelector('main');
     main.classList.remove('menu')
     main.innerHTML = ' ';
-    main.innerHTML = 'How to find us ... <br><img src="images/access.jpg"> Clap your hands together three times, take a deep breath and close your eyes. Breathe out thre times and whisper "Nanka tabetai" while squeezing your buttchecks. When you open your eyes you will find yourself at a table in our restaurant!<br> Or you can just call you and we will explain it you. Its a bit complicated and hidden away in a magical forest... You know. Anyway call us at<br><p class="highlight"> 123-456-789</p><br>You can call us any time. Time does not exist.'
+    main.innerHTML = 'How to find us ... <br><img src="images/access.jpg"> Clap your hands together three times, take a deep breath and close your eyes. Breathe out three times and whisper "Hara hetta" while squeezing your buttcheecks. When you open your eyes you will find yourself at a table in our restaurant!<br> Or you can just call you and we will explain it you. Its a bit complicated and hidden away in a magical forest... You know. Anyway call us at<br><p class="highlight"> 123-456-789</p><br>You can call us any time. Time does not exist.'
 }
 
 export default access;
